11778d75ab85190a5f1908952461e73fc9614b2d
Ash <plase024@morris.umn.edu>
2022-09-06 22:56:08 -0500

    Finish Regex and Fix Testswe

--- regex/regex.sh
+++ regex/regex.sh
@@ -1,6 +1,6 @@
- [ $# -eq 0 ] && exit
-# filename=$( echo "$1" | sed 's/input/output/g' )
-echo "$filename"
-
-# awk 'match($0, /([a-zA-Z\.]+)[\.,] (My favorite sandwich is)? ([^\.]*)\.?$/, groups) {print "1. " groups[1] "\n" "2. " groups[3] "\n" }' "$1" > "$filename"
-echo "Hi" > "r0_output.txt"
+if [[ ! -f "$1" ]]
+then
+	exit
+fi
+filename=$( echo "$1" | sed 's/input/output/g' )
+cat "$1" | sed 's/\. f/\., f/g' | sed 's/\. t/\., t/g' | sed 's/\. M/, M/' | awk 'match($0, /([a-zA-Z\.]+), (My favorite sandwich is )?([^I\.]+)\.?$/, groups) {print "1. " groups[1] "\n" "2. " groups[3] "\n" }' > "$filename" 
